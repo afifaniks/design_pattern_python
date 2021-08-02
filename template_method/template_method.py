1f68e7029d6f11836c59095cb195fcc3a5555cde
Afif Al Mamun <afifanik@gmail.com>
2021-08-02 22:48:19 +0600

    Template Method Pattern

---
+++ template_method/template_method.py
@@ -0,0 +1,50 @@
+from abc import ABCMeta, abstractmethod
+
+
+class SendEmail(metaclass=ABCMeta):
+    @abstractmethod
+    def verify_email(self, email_from, email_to):
+        pass
+
+    @abstractmethod
+    def validate(self, body):
+        pass
+
+    @abstractmethod
+    def send(self, email_from, email_to, body):
+        pass
+
+    def send_email(self, email_from, email_to, body):
+        self.verify_email(email_from, email_to)
+        self.validate(body)
+        self.send(email_from, email_to, body)
+
+
+class GmailSender(SendEmail):
+    def verify_email(self, email_from, email_to):
+        print("Email Verified")
+
+    def validate(self, body):
+        print("Email validated")
+
+    def send(self, email_from, email_to, body):
+        print("Sending ...")
+
+
+class YmailSender(SendEmail):
+    def verify_email(self, email_from, email_to):
+        print("Y Email Verified")
+
+    def validate(self, body):
+        print("Y Email validated")
+
+    def send(self, email_from, email_to, body):
+        print("Y Sending ...")
+
+
+gmail = GmailSender()
+gmail.send_email("xx@ymail.com", "gg@ez.com", "body")
+
+
+ymail = YmailSender()
+ymail.send_email("xx@ymail.com", "gg@ez.com", "body")
